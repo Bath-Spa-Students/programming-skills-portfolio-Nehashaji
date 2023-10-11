d6ef70e5cadcfb870ee39403cc5001a79150d0e0
Nehashaji <144330515+Nehashaji@users.noreply.github.com>
2023-10-11 21:52:51 +0400

    Update Practice 3.py

--- Chapter 1- Getting Started with Python/Exercises/Practice 3.py
+++ Chapter 1- Getting Started with Python/Exercises/Practice 3.py
@@ -1,2 +1,4 @@
 # you need to make a program for a leader board. The program needs to output the number s 1 to 9 .
 # each on separate line followed by a dot .
+
+print("""1.\n2.\n3.\n4.\n5.\n6.\n7.\n8.\n9.""")
