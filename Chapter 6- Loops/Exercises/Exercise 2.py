9ec7af1e0b21dd9d4077f4d61a61e6f7b697cc13
Nehashaji <144330515+Nehashaji@users.noreply.github.com>
2023-11-17 19:43:06 +0400

    Chapter 6.py

--- Chapter 6- Loops/Exercises/Exercise 2.py
+++ Chapter 6- Loops/Exercises/Exercise 2.py
@@ -2,12 +2,18 @@
 #they are between 3 and 12, the ticket is $10; and if they are over age 12, the ticket is $15. Write a loop in which you ask users their 
 #age, and then tell them the cost of their movie ticket.
 
-age=input("Enter Your Age:")
+var = "Your age"
+var += "\n Enter 'quit' when you'r finished. "
 
-for ages in age:
-    if age<3:
-        print("Congrats! your ticket is free")
-    elif age>3 and age<12:
-        print("Your ticket cost is $15 each")
-    else:
-        print("Your Tickect is $20 each")
+while True:
+    age = input(var)
+    if age== 'quit':
+        break
+    age = int(age)
+
+    if age < 3:
+        print("Free entry")
+    elif age <12:
+        print("Ticket charge $10 ")
+    else: 
+        print("Ticket charge $15")
--- Chapter 6- Loops/Exercises/Exercise 5.py
+++ Chapter 6- Loops/Exercises/Exercise 5.py
@@ -2,18 +2,22 @@
 #near the beginning of your program to print a message saying the deli has run out of pastrami, and then use a while loop to remove all 
 #occurrences of 'pastrami' from sandwich_orders. Make sure no pastrami sandwiches end up in finished_sandwiches.
 
-sandwich_orders= ['Chicken' , 'Pastrami' , 'Cheese', 'Club', 'Pastrami', 'Beef' , 'Zinger' , 'Pastrami' ]
+sandwich_orders = [
+    'pastrami', 'Chicken', 'Cheese', 'pastrami',
+    'Club', 'Beef', 'pastrami']
+finished_sandwiches = []
 
 print("Sorry, We run out of pastrami .")
-
-while 'pastrami' in sandwich_orders: 
+while 'pastrami' in sandwich_orders:
     sandwich_orders.remove('pastrami')
 
-while 'pastrami' in sandwich_orders:
-   current_order = sandwich_orders.pop(0)
-   print(f"I made your {current_order} sandwich.")
-   finished_sandwiches.append(current_order)
+print("\n")
+while sandwich_orders:
+    current_sandwich = sandwich_orders.pop()
+    print("I made your " + current_sandwich + " sandwich.")
+    finished_sandwiches.append(current_sandwich)
 
-print("\nList of finished sandwiches:")
+print("\n")
 for sandwich in finished_sandwiches:
-    print(sandwich)
+    print("I made a " + sandwich + " sandwich.")
+
